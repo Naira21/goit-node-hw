ccdddda36702b4d0368502f280013e108406f2a5
Naira Kazeian <nkazeian@gmail.com>
2021-12-19 14:39:51 +0200

    rewrite invokeAction call

--- hw-1/index.js
+++ hw-1/index.js
@@ -1,46 +1,46 @@
-const colors = require('colors')
-const argv = require('yargs').argv;
+const colors = require("colors");
+const argv = require("yargs").argv;
 const {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact
-} = require('./contacts')
-
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-    switch (action) {
-        case 'list':
-            const contacts = await listContacts()
-            console.table(contacts)
-        break
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
 
-        case 'get':
-            const contactById = await getContactById(id)
-            if (contactById) {
-                console.log('We have found the contact'.zebra)
-                console.log(contactById)
-                return
-            }      
-                console.log('Error! Identification failed'.red)
-           
-        break
+    case "get":
+      const contactById = await getContactById(id);
+      if (contactById) {
+        console.log("We have found the contact".zebra);
+        console.log(contactById);
+        return;
+      }
+      console.log("Error! Identification failed".red);
 
-        case 'add':
-            const newContact = await addContact(name, email, phone)
-            console.log(colors.rainbow('New contact is created'))
-            console.log(newContact)
-        break
+      break;
 
-        case 'remove':
-            const refreshedBase = await removeContact(id)
-            console.table(refreshedBase)
-        break
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(colors.rainbow("New contact is created"));
+      console.log(newContact);
+      break;
 
-        default:
-            console.warn('Unknown action type!'.red.bgCyan)
-    }
-}
+    case "remove":
+      const refreshedBase = await removeContact(id);
+      console.table(refreshedBase);
+      break;
 
-invokeAction(argv)
+    default:
+      console.warn("Unknown action type!".red.bgCyan);
+  }
+};
 
+(async () => {
+  await invokeAction(argv);
+})();
